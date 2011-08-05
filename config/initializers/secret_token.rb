255abb9f32842fa849b2a577685a2f5512055a2d
Ximing Yu <ximing.yu@gmail.com>
2011-08-05 15:15:21 -0700

    Initial import with a default rails 3 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NoiBt::Application.config.secret_token = '3b8ed908854f2f3d4491bf637368b1420f45e3071100dfd8b7e20d34bf4facd012476a7e93ba6e2267703a7f7dd68fc3e927db1b4153103228d3413b7269a8c4'
